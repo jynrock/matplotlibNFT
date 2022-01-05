53c9685895e910fd0ad45dc7953566485e4b8340
jynrock <77015465+jynrock@users.noreply.github.com>
2022-01-05 01:09:07 +0100

    Update voxels_2.py

--- voxels_2.py
+++ voxels_2.py
@@ -10,27 +10,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random as rd
 
-
-# prepare some coordinates
-x, y, z = np.indices((24, 24, 24))
-
-# draw cuboids in the top left and bottom right corners, and a link between
-# them
-cube1 = (x < rd.randint(0,24)) & (y < rd.randint(0,24)) & (z < rd.randint(0,24))
-cube2 = (x >= rd.randint(0,24)) & (y >= rd.randint(0,24)) & (z >= rd.randint(0,24))
-link = abs(x - y) + abs(y - z) + abs(z - x) <= 2
-
-# combine the objects into a single boolean array
-voxelarray = cube1 | cube2 | link
-
-# set the colors of each object
-colors = np.empty(voxelarray.shape, dtype=object)
-colors[link] = 'red'
-colors[cube1] = 'blue'
-colors[cube2] = 'green'
-
-# and plot everything
-ax = plt.figure().add_subplot(projection='3d')
-ax.voxels(voxelarray, facecolors=colors, alpha=0.5)
-plt.axis('off')
-plt.savefig('books_read.png', transparent=True)
+i = 0
+while i < 5:
+	name = i +1
+	# prepare some coordinates
+	x, y, z = np.indices((24, 24, 24))
+	
+	# draw cuboids in the top left and bottom right corners, and a link between
+	# them
+	cube1 = (x < rd.randint(0,24)) & (y < rd.randint(0,24)) & (z < rd.randint(0,24))
+	cube2 = (x >= rd.randint(0,24)) & (y >= rd.randint(0,24)) & (z >= rd.randint(0,24))
+	link = abs(x - y) + abs(y - z) + abs(z - x) <= 1
+	
+	# combine the objects into a single boolean array
+	voxelarray = cube1 | cube2 | link
+	
+	list=['green','blue','yellow','red','purple']
+	# set the colors of each object
+	colors = np.empty(voxelarray.shape, dtype=object)
+	colors[link] = rd.choice(list)
+	colors[cube1] = rd.choice(list)
+	colors[cube2] = rd.choice(list)
+	
+	# and plot everything
+	ax = plt.figure().add_subplot(projection='3d')
+	ax.voxels(voxelarray, facecolors=colors, alpha=0.5)
+	plt.axis('off')
+	plt.savefig('NFT_%s.png' % name, transparent=True)
+	i = i +1
